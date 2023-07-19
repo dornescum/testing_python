18211f616d271e64603f0bb2bb8724910c179228
dornescu mihai <dornescu@gmail.com>
2023-07-19 21:10:30 +0300

    indent vs code vs pycharm

--- youtube/api_requests.py
+++ youtube/api_requests.py
@@ -7,8 +7,8 @@ baseurl = 'https://rickandmortyapi.com/api/'
 endpoint = 'character'
 
 def main_request(baseurl, endpoint, x):
-      r =  requests.get(baseurl + endpoint + f'?page={x}')
-      return r.json()
+    r =  requests.get(baseurl + endpoint + f'?page={x}')
+    return r.json()
 
 
 def get_pages(response):
@@ -17,19 +17,19 @@ def get_pages(response):
 
 
 def parse_json(response):
-     charlist = []
-     for item in response['results']:
-          # print(item)
+    charlist = []
+    for item in response['results']:
+      # print(item)
       # name = response['results'][0]['name']
       # episodes = response['results'][0]['episode']
-          char = {
-          'id' : item['id'],
-          'name': item['name'],
-          'no_ep': len(item['episode'])
-          # 'no_ep': item['episode']
-          }
-          charlist.append(char)
-     return charlist
+        char = {
+        'id' : item['id'],
+        'name': item['name'],
+        'no_ep': len(item['episode'])
+        # 'no_ep': item['episode']
+        }
+        charlist.append(char)
+    return charlist
 
 
 # print(r.json())
@@ -37,7 +37,7 @@ mainlist = []
 data = main_request(baseurl, endpoint, 1)
 for x in range(1, get_pages(data)+1):
       # print(x)
-      mainlist.extend(parse_json(main_request(baseurl, endpoint, x)))
+    mainlist.extend(parse_json(main_request(baseurl, endpoint, x)))
 
 
 df = pd.DataFrame(mainlist)
